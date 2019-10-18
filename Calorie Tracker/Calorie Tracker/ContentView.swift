c2d2aee0eb11de2098e0b1555ff4f93cdd7741e1
William <chen.will7@gmail.com>
2019-10-18 08:20:47 -0700

    Installed Swift Chart

---
+++ Calorie Tracker/Calorie Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calorie Tracker
+//
+//  Created by William Chen on 10/18/19.
+//  Copyright © 2019 William Chen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
